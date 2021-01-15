d72fe4c28830bce8e64fbeb7ce03064a086bb28c
George-Rhodes <george_rhodes@hotmail.co.uk>
2021-01-15 08:56:56 +0000

    change on wellcome

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd K8s/
-
+sleep 4s
 #docker login
 #echo $BUILD_NUMBER
 sudo kubectl apply -f ./backend/backend.yaml
@@ -9,5 +9,5 @@ sudo kubectl apply -f ./database/databasepersistent.yaml
 sudo kubectl apply -f ./front-end/front-end.yaml
 sudo kubectl apply -f ./nginx/nginx.yaml
 
-#kubectl rollout restart -f deployment.ap/backend
-#kubectl rollout restart -f default/frontend
+kubectl rollout restart deployment.apps/backend
+kubectl rollout restart deployment.apps/frontend
