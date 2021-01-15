d1a9043b79d06164207ea7d4c7c54471300aca7a
George-Rhodes <george_rhodes@hotmail.co.uk>
2021-01-15 08:21:41 +0000

    revert back to latest and hopefully make rolling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd K8s/
 
-docker login
-echo $BUILD_NUMBER
+#docker login
+#echo $BUILD_NUMBER
 sudo kubectl apply -f ./backend/backend.yaml
 sudo kubectl apply -f ./database/database.yaml
 sudo kubectl apply -f ./database/databasepersistent.yaml
 sudo kubectl apply -f ./front-end/front-end.yaml
 sudo kubectl apply -f ./nginx/nginx.yaml
 
-#kubectl rollout restart -f default/backend
+#kubectl rollout restart -f deployment.ap/backend
 #kubectl rollout restart -f default/frontend
